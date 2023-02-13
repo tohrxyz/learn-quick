1fc030d5f442a88962fee833ad7580716f6e52e2
tomashrib <tomas.hrib@proton.me>
2023-02-13 22:43:22 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  const resetCount = () => setCount(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.count}>{count}</Text>
+      <View style={styles.row}>
+        <Button title="Subtract" onPress={decreaseCount} />
+        <Button title="Reset" onPress={resetCount} />
+        <Button title="Add" onPress={increaseCount} />
+      </View>
     </View>
   );
 }
@@ -13,8 +24,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '80%',
+  },
+  count: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  }
 });
+
+
