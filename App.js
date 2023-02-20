380879bc0f2c5cb5719c29499438ac6301954699
tomashrib <tomas.hrib@proton.me>
2023-02-20 12:22:08 +0100

    Added basic layout

--- App.js
+++ App.js
@@ -1,42 +1,75 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import { StyleSheet } from 'react-native';
+import { View, Text, Button , TextInput} from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
-  const increaseCount = () => setCount(count + 1);
-  const decreaseCount = () => setCount(count - 1);
-  const resetCount = () => setCount(0);
+  const [value, onChangeText] = useState('Placeholder')
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.count}>{count}</Text>
-      <View style={styles.row}>
-        <Button title="Subtract" onPress={decreaseCount} />
-        <Button title="Reset" onPress={resetCount} />
-        <Button title="Add" onPress={increaseCount} />
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={dismissKeyboard}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>Learn-Quick</Text>
+          <Text style={styles.description}>Enter text to be shortened into bullet points:</Text>
+          <View style={styles.textInputContainer}>
+              <TextInput
+                editable
+                multiline
+                numberOfLines={4}
+                //maxLenght
+                onChangeText={text => onChangeText(text)}
+                value = {value}
+                style={styles.textInput}
+              /> 
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // marginTop: 50,
+    // justifyContent: 'center',
+    alignItems: 'center'
   },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    width: '80%',
-  },
-  count: {
-    fontSize: 32,
+  title:{
+    textAlign: 'center',
+    fontSize: 40,
     fontWeight: 'bold',
-    marginBottom: 16,
+    color: '#0277bd',
+  },
+  description:{
+    marginTop: 150,
+    fontSize: 25,
+    maxWidth: 300,
+    // color: '#bdbdbd'
+    // marginLeft: -50,
+  },
+  textInput:{
+    color: 'black',
+    fontSize: 18,
+  },
+  textInputContainer:{
+    borderWidth: 3,
+    borderColor: '#0277bd',
+    borderRadius: 10,
+    marginTop: 30,
+    width: 350,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    minHeight: 150,
+  },
+  contentContainer:{
+    paddingHorizontal: 20,
+    paddingVertical: 15,
   }
 });
 
