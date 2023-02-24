b91af948c87d4716da3dca7f72664dbdaaca17be
tomashrib <tomas.hrib@proton.me>
2023-02-24 21:49:05 +0100

    removes whitespace at the end of textarea input

--- App.js
+++ App.js
@@ -20,36 +20,35 @@ export default function App() {
     // safeareaview - fixes cover up by iphone notch etc. -> not displayed under it
     <SafeAreaView style={styles.container}>
  
-      <TouchableWithoutFeedback onPress={dismissKeyboard}>
-        <View style={styles.contentContainer}>
-          
-          <Text style={styles.title}>Learn-Quick</Text>
-          {/* <Text style={styles.description}>Summarize text</Text> */}
-          
-          <View style={styles.textInputContainer}>
-              <TextInput
-                placeholder="Enter text to be summarized"
-                editable
-                multiline
-                numberOfLines={4}
-                onChangeText={text => onChangeText(text)}
-                value = {value}
-                returnKeyType="done"
-                onSubmitEditing={dismissKeyboard}
-                style={styles.textInput}
-              /> 
-          </View>
-          
-          <View style={styles.buttonSummarizeContainer}>
-            <Button
-              title="Summarize !" 
-              color="white"
-              // fontSize={30} -> cant change fontSize for iOS -> solution: create custom button
-            />
-          </View>
+      <View style={styles.contentContainer}>
+        
+        <Text style={styles.title}>Learn-Quick</Text>
+        
+        <View style={styles.textInputContainer}>
+            <TextInput
+              placeholder="Enter text to be summarized"
+              editable
+              multiline
+              numberOfLines={10}
+              // this handles input text change
+              // also removes whitespace on the end of textarea
+              onChangeText={text => onChangeText(text.trimEnd())}
+              value = {value}
+              returnKeyType={'done'}
+              onSubmitEditing={dismissKeyboard}
+              style={styles.textInput}
+            /> 
         </View>
-      </TouchableWithoutFeedback>
-      
+        
+        <View style={styles.buttonSummarizeContainer}>
+          <Button
+            title="Summarize !" 
+            color="white"
+            // fontSize={30} -> cant change fontSize for iOS -> solution: create custom button
+          />
+        </View>
+      </View>
+
     </SafeAreaView>
   );
 }
